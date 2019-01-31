ba02ec6a5d5b670466a97009c1b8ad6ee1de1336
Bjarne Bülow <bjarne.bulow@xlent.se>
2019-01-31 17:03:24 +0100

    some more...

--- src/App.js
+++ src/App.js
@@ -9,59 +9,15 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			input: "",
 			showPopup: false,
-			layoutName: "numeric",
-			input: ""
 		};
 	};
-
-	display = {
-		'{bksp}': 'RADERA',
-	};
-
-	numLayout = {
-		'numeric': [
-			'`1 2 3',
-			'4 5 6',
-			'7 8 9',
-			'0 {bksp}'
-			],
-		};
-	// 'default': [
-	// 	'` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
-	// 	'{tab} q w e r t y u i o p [ ] \\',
-	// 	'{lock} a s d f g h j k l ; \' {enter}',
-	// 	'{shift} z x c v b n m , . / {shift}',
-	// 	'.com @ {space}'
-	// 	],
-	// 'shift': [
-	// 	'~ ! @ # $ % ^ & * ( ) _ + {bksp}',
-	// 	'{tab} Q W E R T Y U I O P { } |',
-	// 	'{lock} A S D F G H J K L : " {enter}',
-	// 	'{shift} Z X C V B N M < > ? {shift}',
-	// 	'.com @ {space}'
-	// 	]
 
 	togglePopup() {
 		this.setState({
 			showPopup: !this.state.showPopup
 		});
-	};
-
-	onChange = input => {
-		this.setState({
-			input: input
-		});
-		console.log("Input changed", input);
-	};
-
-	onKeyPress = button => {
-		console.log("Button pressed", button);
-
-		/**
-		 * If you want to handle the shift and caps lock buttons
-		 */
-		if (button === "{shift}" || button === "{lock}") this.handleShift();
 	};
 
 	handleShift = () => {
@@ -74,54 +30,111 @@ class App extends Component {
 
 	onChangeInput = event => {
 		let input = event.target.value;
+		if (this.state.showPopup) {
+			this.setState(
+				{
+					input: input,
+				},
+				() => {
+					this.popupKeyboard.keyboard.setInput(input);
+				}
+			);
+		} else {
+			this.setState(
+				{
+					input: input,
+				}
+			);
+		}
+	};
+
+	inputField = event => {
 		this.setState(
 			{
-				input: input
-			},
-			() => {
-				this.keyboard.setInput(input);
+				input: event,
 			}
 		);
+		console.log('InputUpdate:', event, this.state.input);
 	};
+
+	doBlur = (e) => {
+			e.target.blur();
+	}
 
 	render() {
     return (
       <div className="App">
 				<img src={logo} className="App-logo" alt="logo" />
-				<button onClick={this.togglePopup.bind(this)}>show popup</button>
-				<button onClick={() => {alert('woooooooot?');}}>try me when popup is open</button>
+				<button onClick={()=>this.togglePopup()}>show popup</button>
 				<input
+					onFocus={this.doBlur}
 					value={this.state.input}
 					placeholder={"Tap on the virtual keyboard to start"}
 					onChange={e => this.onChangeInput(e)}
 				/>
 				{this.state.showPopup ?
-					<Popup
-						text='Close Me'
-						closePopup={this.togglePopup.bind(this)}
+					<PopupKeyboard
+						ref={r => (this.popupKeyboard = r)}
+						closePopup={(e)=>this.togglePopup(e)}
+						inputField={(e)=>this.inputField(e)}
 					/>
 					: null
 				}
-				<Keyboard
-					ref={r => (this.keyboard = r)}
-					layoutName={this.state.layoutName}
-					layout={this.numLayout}
-					onChange={input => this.onChange(input)}
-					onKeyPress={button => this.onKeyPress(button)}
-					display={this.display}
-				/>
       </div>
     );
   }
 }
 
-class Popup extends React.Component {
+class PopupKeyboard extends React.Component {
+
+	display = {
+		'{bksp}': 'RADERA',
+	};
+
+	numLayout = {
+		'numeric': [
+			'`1 2 3',
+			'4 5 6',
+			'7 8 9',
+			'0 {bksp}'
+		],
+	};
+
+	onKeyPress = button => {
+		console.log("Button pressed", button);
+
+		/**
+		 * If you want to handle the shift and caps lock buttons
+		 */
+		if (button === "{shift}" || button === "{lock}") this.handleShift();
+	};
+
+	onChange = input => {
+		console.log("Input", input);
+		this.props.inputField(input);
+	};
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			layoutName: "numeric",
+		};
+	};
+
 	render() {
 		return (
-			<div className='popup'>
+			<div className='popup' draggable={false}>
 				<div className='popup_inner'>
-					<h1>{this.props.text}</h1>
-					<button onClick={this.props.closePopup}>close me</button>
+					<Keyboard
+						ref={r => (this.keyboard = r)}
+						layoutName={this.state.layoutName}
+						layout={this.numLayout}
+						onChange={input => this.onChange(input)}
+						onKeyPress={button => this.onKeyPress(button)}
+						display={this.display}
+						useTouchEvents={false}
+					/>
+					<button onClick={this.props.closePopup}>X</button>
 				</div>
 			</div>
 		);
